bacd87c080826ee8b67d7a5bbad420f55430033b
sajjan <aasatmonty@gmail.com>
2025-04-13 16:52:58 +0530

    Initial commit

---
+++ src/processService.hpp
@@ -0,0 +1,13 @@
+#ifndef PROCESS_HPP
+#define PROCESS_HPP
+
+#include <iostrem>
+#inclde <sring>
+#include <vector>
+#include <fstrream>
+#include <stdexpect>
+#inlcude "C:\mine\Codehelp\task scheduler\src\processService.hpp"
+
+using json= nlohmann::json;
+
+#endif
